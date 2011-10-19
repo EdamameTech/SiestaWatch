32a2729825a853949a24c5191363dd00c2ba981d
zunda <zunda at freeshell.org>
2011-10-18 23:35:46 -1000

    Passes the test to hop between summer time and winter time

--- SiestaWatchTest/src/com/edamametech/android/SiestaWatch/test/SiestaWatchConfTestCase.java
+++ SiestaWatchTest/src/com/edamametech/android/SiestaWatch/test/SiestaWatchConfTestCase.java
@@ -17,7 +17,9 @@ import com.edamametech.android.SiestaWatch.SiestaWatchConf;
 import android.content.Context;
 import android.net.Uri;
 import android.test.AndroidTestCase;
+import android.util.Log;
 
+import java.util.Calendar;
 import java.util.TimeZone;
 
 public class SiestaWatchConfTestCase extends AndroidTestCase {
@@ -84,7 +86,42 @@ public class SiestaWatchConfTestCase extends AndroidTestCase {
         assertEquals(target, SiestaWatchConf.timeLimitMillis(mContext, current, tz));
     }
 
-    // TODO: transition between summer time and winter time
+    public void testTimeLimitMillisFromSummerToWinter() {
+        long current = 1319905800000L; // 2011-10-29 18:30:00 CEST
+        long target = 1319981400000L; // 2011-10-30 14:30:00 CET
+        TimeZone tz = TimeZone.getTimeZone("Europe/Berlin");
+        SiestaWatchConf.setTimeLimitHour(mContext, 14);
+        SiestaWatchConf.setTimeLimitMinute(mContext, 30);
+        long calculated = SiestaWatchConf.timeLimitMillis(mContext, current, tz);
+
+        showTimes("FromSummerToWinter", current, target, calculated, tz);
+        assertEquals(target, calculated);
+    }
+
+    public void testTimeLimitMillisFromWinterToSummer() {
+        long current = 1301160600000L; // 2011-03-26 18:30:00 CET
+        long target = 1301229000000L; // 2011-03-27 14:30:00 CEST
+        TimeZone tz = TimeZone.getTimeZone("Europe/Berlin");
+        SiestaWatchConf.setTimeLimitHour(mContext, 14);
+        SiestaWatchConf.setTimeLimitMinute(mContext, 30);
+        long calculated = SiestaWatchConf.timeLimitMillis(mContext, current, tz);
+
+        showTimes("FromSummerToWinter", current, target, calculated, tz);
+        assertEquals(target, calculated);
+    }
+
+    private void showTimes(String tag, long current, long target, long calculated, TimeZone tz) {
+        Log.v(tag, "Timezone: " + tz.getDisplayName());
+        Calendar cal = Calendar.getInstance();
+        cal.setTimeZone(tz);
+        cal.setTimeInMillis(current);
+        Log.v(tag, "Current: " + cal.getTime().toString());
+        cal.setTimeInMillis(target);
+        Log.v(tag, "Target:  " + cal.getTime().toString());
+        cal.setTimeInMillis(calculated);
+        Log.v(tag, "Calc'ed: " + cal.getTime().toString());
+
+    }
 
     public void testNeedsTimeLimit() {
         boolean orig = SiestaWatchConf.needsTimeLimit(mContext);
