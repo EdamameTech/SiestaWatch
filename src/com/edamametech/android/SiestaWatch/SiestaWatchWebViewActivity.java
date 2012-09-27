fff5def6714a505e63dcbd995dc4e760a9a2d346
zunda <zunda at freeshell.org>
2012-09-26 19:22:38 -1000

    Modified name of a variable to follow AOSP style

--- src/com/edamametech/android/SiestaWatch/SiestaWatchWebViewActivity.java
+++ src/com/edamametech/android/SiestaWatch/SiestaWatchWebViewActivity.java
@@ -23,7 +23,7 @@ public class SiestaWatchWebViewActivity extends Activity {
 	private static final boolean DEBUG = (LOGLEVEL > 0);
 	private static final String LogTag = "SiestaWatchWebViewActivity";
 
-	WebView webView = null;
+	WebView mWebView = null;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -33,15 +33,15 @@ public class SiestaWatchWebViewActivity extends Activity {
 			Log.v(LogTag, "onCreate()");
 
 		setContentView(R.layout.webview);
-		webView = (WebView) findViewById(R.id.webView);
+		mWebView = (WebView) findViewById(R.id.webView);
 
-		webView.loadUrl(getIntent().getData().toString());
+		mWebView.loadUrl(getIntent().getData().toString());
 	}
 
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
-		if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
-			webView.goBack();
+		if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {
+			mWebView.goBack();
 			return true;
 		}
 		return super.onKeyDown(keyCode, event);
